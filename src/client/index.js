8dc4011a6d3b05feb87af0b48c6354b3fdd71abd
Dan Brower <danielsbrower@gmail.com>
2020-12-27 15:24:49 -0800

    seems to be working

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,15 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import { onBlur } from './js/formHandler'
 
-console.log(checkForName);
+import './styles/resets.css'
+import './styles/base.css'
+import './styles/form.css'
+import './styles/footer.css'
+import './styles/header.css'
 
-alert("I EXIST")
-console.log("CHANGE!!");
+export {
+    checkForName,
+    handleSubmit,
+    onBlur
+}
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,14 +3,36 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
+    fetch('http://localhost:8080/apiKey')
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        return res.json()
+    })
+    .then(function(json) {
+        let mood = getMood(formText, json.key)
+        return mood;
+    })
+    .then( function(mood) {
+        document.getElementById('results').innerHTML = mood.subjectivity
     })
 }
 
+const getMood = async (targetURL, key)=>{
+
+    const str = `https://api.meaningcloud.com/sentiment-2.1?key=${key}&lang=en&url=${targetURL}`
+      const res = await fetch(str)
+    try {
+      const data = await res.json();
+      return data;
+    }  catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+}
+
+function onBlur(event) {
+
+}
+
 export { handleSubmit }
+export { onBlur }
